6810e1d96564a449d691387649b9ac8fe72afe55
irukahaoruka <erika03301109@gmail.com>
2025-04-28 12:48:55 +0900

    firstcommit

---
+++ InstantSearch iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InstantSearch iOS
+//
+//  Created by 櫻井絵理香 on 2025/04/25.
+//
+
+import SwiftUI
+import InstantSearchSwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstantSearch iOS/InstantSearch_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantSearch_iOSApp.swift
+//  InstantSearch iOS
+//
+//  Created by 櫻井絵理香 on 2025/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InstantSearch_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
